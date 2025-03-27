d5f0553d0bd77ad444dadeedf4cdfdd945113e84
Fabio Gontijo <fabiojrgontijodev@gmail.com>
2025-03-26 22:12:34 -0300

    implementing existing account validation

--- src/main/java/com/fabiogontijo/bank_management_api/account/AccountCommand.java
+++ src/main/java/com/fabiogontijo/bank_management_api/account/AccountCommand.java
@@ -1,5 +1,6 @@
 package com.fabiogontijo.bank_management_api.account;
 
+import com.fabiogontijo.bank_management_api.core.BankManagementMessageSource;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +14,14 @@ public class AccountCommand {
 
     private final AccountRepository repository;
 
+    private final BankManagementMessageSource messageSource;
+
     public Account create(Account account) {
+        repository.findById(account.getAccountNumber()).ifPresent(existingAccount -> {
+            throw new IllegalArgumentException(
+                    messageSource.getMessage("account.already.exists", new Object[]{account.getAccountNumber()})
+            );
+        });
         return repository.save(Account.of(account));
     }
 
